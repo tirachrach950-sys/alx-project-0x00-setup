7e8fe122a34b7ace0a6c29116f3bf7b81c3a2ba2
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 16:20:29 +0100

    Update index.ts

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,6 +1,9 @@
-export interface PillProps {
+export interface ButtonProps {
   title: string;
+  styles: string; // full TailwindCSS string for size/shape/colors
+  onClick?: () => void;
 }
+
 export interface ButtonProps {
   title: string;
   size?: 'small' | 'medium' | 'large';
