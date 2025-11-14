f8450d4df4760096323c6e1772f3676cfefa40df
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-14 16:13:17 +0100

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -11,4 +11,23 @@ const Landing: React.FC = () => {
 };
 
 export default Landing;
+import React from "react";
+import Card from "@/components/Card";
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+      <div className="p-2 flex gap-2 mt-2">
+        <Card>
+          <Pill title="Top Villa" />
+          <Pill title="Self CheckIn" />
+          <Pill title="Free Reschedule" />
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
 
